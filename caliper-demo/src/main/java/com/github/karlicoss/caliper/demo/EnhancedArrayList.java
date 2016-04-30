50177cb48e801b684dbd5e69db5647d6f6ba2def
Dima Gerasimov <karlicoss@gmail.com>
2016-04-30 04:16:17 +0300

    More types; formatting and prettification

--- caliper-demo/src/main/java/com/github/karlicoss/caliper/demo/EnhancedArrayList.java
+++ caliper-demo/src/main/java/com/github/karlicoss/caliper/demo/EnhancedArrayList.java
@@ -1,9 +1,13 @@
 package com.github.karlicoss.caliper.demo;
 
+import android.content.Intent;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class EnhancedArrayList extends EnhancedIterable {
+
+    private Intent intent;
 
     private final ArrayList<Integer> data;
 
